584a1bc25630678df6b23b2738dafb845cb02b77
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-03 07:03:05 -0500

    03B Done

--- lib/day03.ex
+++ lib/day03.ex
@@ -11,7 +11,48 @@ defmodule AdventOfCode2024.Day03 do
     |> Enum.map(fn [_full_match | num_strings] -> Enum.map(num_strings, &String.to_integer/1) end)
   end
 
-  def part_b(_lines) do
+  def part_b(text) do
+    text
+    |> ignore_some_text()
+    |> part_a()
+  end
+
+  def ignore_some_text(text) do
+    do_and_dont_indexes_set =
+      Regex.scan(~r/do/, text, return: :index)
+      |> Enum.map(fn [{index, _}] -> index end)
+      |> MapSet.new()
+
+    dont_indexes_set =
+      Regex.scan(~r/don't/, text, return: :index)
+      |> Enum.map(fn [{index, _}] -> index end)
+      |> MapSet.new()
+
+    do_indexes_set = MapSet.difference(do_and_dont_indexes_set, dont_indexes_set)
+
+    do_indexes = [0 | Enum.sort(do_indexes_set)]
+    dont_indexes = Enum.sort(dont_indexes_set)
+
+    do_sections(text, do_indexes, dont_indexes)
+  end
+
+  defp do_sections(text, [do_head | _do_tail], [] = _dont) do
+    String.slice(text, do_head..-1//1)
+  end
+
+  defp do_sections(text, [do_head | do_tail], [dont_head | dont_tail]) do
+    partial_text = String.slice(text, do_head..(dont_head - 1))
+
+    next_do = Enum.drop_while(do_tail, &(&1 < dont_head))
+
+    case next_do do
+      [] ->
+        partial_text
+
+      [next_do_head | _] ->
+        next_dont = Enum.drop_while(dont_tail, &(&1 < next_do_head))
+        partial_text <> do_sections(text, next_do, next_dont)
+    end
   end
 
   def a() do
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -8,22 +8,19 @@ defmodule AdventOfCode2024.Day03Test do
   end
 
   defp example_input() do
-    """
-    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-    """
-    |> String.trim()
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
   end
 
   test "parse simple input to numbers" do
-    assert AdventOfCode2024.Day03.parse_numbers("mul(2,3)") == [[2, 3]]
+    assert Day03.parse_numbers("mul(2,3)") == [[2, 3]]
   end
 
   test "parse input with junk" do
-    assert AdventOfCode2024.Day03.parse_numbers("aksjdflasdfjmul(7,11)aksjdflasdfj") == [[7, 11]]
+    assert Day03.parse_numbers("aksjdflasdfjmul(7,11)aksjdflasdfj") == [[7, 11]]
   end
 
   test "parse input with multiple muls" do
-    assert AdventOfCode2024.Day03.parse_numbers("mul(1,2),mul(3,4)&mul(5,6)+mul[7,8]") == [[1, 2], [3, 4], [5, 6]]
+    assert Day03.parse_numbers("mul(1,2),mul(3,4)&mul(5,6)+mul[7,8]") == [[1, 2], [3, 4], [5, 6]]
   end
 
   test "parse input across lines" do
@@ -31,11 +28,30 @@ defmodule AdventOfCode2024.Day03Test do
            mul(1,2),mul(3,4)abc
            other_mul(5,6)other
            """
-           |> AdventOfCode2024.Day03.parse_numbers() == [[1, 2], [3, 4], [5, 6]]
+           |> Day03.parse_numbers() == [[1, 2], [3, 4], [5, 6]]
   end
 
-  @tag :skip
   test "Day03 part B example" do
-    assert Day03.part_b(example_input()) == :something_else
+    assert Day03.part_b(example_input()) == 48
+  end
+
+  test "delete text after don't and including don't" do
+    assert Day03.ignore_some_text("abcdon'tefg") == "abc"
+  end
+
+  test "delete text between don't and do, keep do, but not don't" do
+    assert Day03.ignore_some_text("abcdon'tblahblahdodef") == "abcdodef"
+  end
+
+  test "multiple don't - do sections" do
+    assert Day03.ignore_some_text("adon'tbdocdon'tddoe") == "adocdoe"
+  end
+
+  test "multiple don'ts in a row" do
+    assert Day03.ignore_some_text("adon'tbdon'tcdon'tddoe") == "adoe"
+  end
+
+  test "multiple dos in a row" do
+    assert Day03.ignore_some_text("adobdocdon'tbdon'tcdoddoedofdon'tddoe") == "adobdocdoddoedofdoe"
   end
 end
