9ff263af87ebf963ff7d5f840f862f6b833ed792
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-07 08:07:13 -0500

    Parse Day06 input

--- lib/day06.ex
+++ lib/day06.ex
@@ -4,6 +4,32 @@ defmodule AdventOfCode2024.Day06 do
   def part_a(_lines) do
   end
 
+  def parse_input(lines) do
+    boxes =
+      lines
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {line, y_index} -> boxes_in_line(line, y_index) end)
+      |> MapSet.new()
+
+    {start_location(lines), boxes}
+  end
+
+  defp boxes_in_line(line, y_index) do
+    ~r/#/
+    |> Regex.scan(line, return: :index)
+    |> Enum.map(fn [{x_index, _}] -> {x_index, y_index} end)
+  end
+
+  defp start_location(lines) do
+    y_index = Enum.find_index(lines, &String.contains?(&1, "^"))
+
+    line = Enum.at(lines, y_index)
+
+    [[{x_index, _}]] = Regex.scan(~r/\^/, line, return: :index)
+
+    {x_index, y_index}
+  end
+
   def part_b(_lines) do
   end
 
--- test/day06_test.exs
+++ test/day06_test.exs
@@ -25,6 +25,23 @@ defmodule AdventOfCode2024.Day06Test do
     |> Enum.map(&String.trim/1)
   end
 
+  describe "parse_input/1" do
+    test "creates a starting_location, boxes tuple" do
+      assert Day06.parse_input(example_input()) ==
+               {{4, 6},
+                MapSet.new([
+                  {4, 0},
+                  {9, 1},
+                  {2, 3},
+                  {7, 4},
+                  {1, 6},
+                  {8, 7},
+                  {0, 8},
+                  {6, 9}
+                ])}
+    end
+  end
+
   @tag :skip
   test "Day06 part B example" do
     assert Day06.part_b(example_input()) == :something_else
