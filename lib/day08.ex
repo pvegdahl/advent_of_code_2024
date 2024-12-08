c83c4ad6d9f8372b710e26307f42a5a50bbdba07
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-08 08:27:02 -0500

    Day 08B done

--- lib/day08.ex
+++ lib/day08.ex
@@ -2,7 +2,7 @@ defmodule AdventOfCode2024.Day08 do
   alias AdventOfCode2024.Helpers
 
   def part_a(lines) do
-    do_it_all(lines, &pair_to_antinodes_a/1)
+    do_it_all(lines, &pair_to_antinodes_a/2)
   end
 
   defp do_it_all(lines, pair_to_antinodes_func) do
@@ -18,7 +18,7 @@ defmodule AdventOfCode2024.Day08 do
   def antinodes(antennas, dimensions, pair_to_antinodes_func) do
     antennas
     |> pairs()
-    |> Enum.flat_map(pair_to_antinodes_func)
+    |> Enum.flat_map(fn pair -> pair_to_antinodes_func.(pair, dimensions) end)
     |> Enum.filter(fn point -> Helpers.in_bounds?(point, dimensions) end)
     |> MapSet.new()
   end
@@ -31,20 +31,30 @@ defmodule AdventOfCode2024.Day08 do
     end
   end
 
-  def pair_to_antinodes_a({{x0, y0}, {x1, y1}}) do
+  def pair_to_antinodes_a({{x0, y0}, {x1, y1}}, _dimensions) do
     x_diff = x1 - x0
     y_diff = y1 - y0
     [{x0 - x_diff, y0 - y_diff}, {x1 + x_diff, y1 + y_diff}]
   end
 
   def part_b(lines) do
-    do_it_all(lines, &pair_to_antinodes_b/1)
+    do_it_all(lines, &pair_to_antinodes_b/2)
   end
 
-  def pair_to_antinodes_b({{x0, y0}, {x1, y1}}) do
+  def pair_to_antinodes_b({{x0, y0} = point0, {x1, y1}}, dimensions) do
     x_diff = x1 - x0
     y_diff = y1 - y0
-    [{x0 - x_diff, y0 - y_diff}, {x1 + x_diff, y1 + y_diff}]
+
+    forward_points = points_on_line(point0, {x_diff, y_diff}, dimensions)
+    [_point0 | backwards_points] = points_on_line(point0, {-x_diff, -y_diff}, dimensions)
+
+    backwards_points ++ forward_points
+  end
+
+  defp points_on_line(starting_point, {x_diff, y_diff}, dimensions) do
+    starting_point
+    |> Stream.iterate(fn {x, y} -> {x + x_diff, y + y_diff} end)
+    |> Enum.take_while(fn point -> Helpers.in_bounds?(point, dimensions) end)
   end
 
   def a() do
