8737c86fa3928ad5633d5e9612b425eb4926384e
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-08 08:16:25 -0500

    [REFACTOR] Pull out the one function that differs between A and B

--- lib/day08.ex
+++ lib/day08.ex
@@ -2,23 +2,23 @@ defmodule AdventOfCode2024.Day08 do
   alias AdventOfCode2024.Helpers
 
   def part_a(lines) do
+    do_it_all(lines, &pair_to_antinodes_a/1)
+  end
+
+  defp do_it_all(lines, pair_to_antinodes_func) do
     dimensions = Helpers.grid_dimensions(lines)
 
     lines
     |> Helpers.parse_string_grid()
-    |> Enum.map(fn {_char, antennas} -> antinodes(antennas, dimensions) end)
+    |> Enum.map(fn {_char, antennas} -> antinodes(antennas, dimensions, pair_to_antinodes_func) end)
     |> Enum.reduce(&MapSet.union/2)
     |> MapSet.size()
   end
 
-  def antinodes(antennas, dimensions) do
+  def antinodes(antennas, dimensions, pair_to_antinodes_func) do
     antennas
     |> pairs()
-    |> Enum.flat_map(fn {{x0, y0}, {x1, y1}} ->
-      x_diff = x1 - x0
-      y_diff = y1 - y0
-      [{x0 - x_diff, y0 - y_diff}, {x1 + x_diff, y1 + y_diff}]
-    end)
+    |> Enum.flat_map(pair_to_antinodes_func)
     |> Enum.filter(fn point -> Helpers.in_bounds?(point, dimensions) end)
     |> MapSet.new()
   end
@@ -31,7 +31,20 @@ defmodule AdventOfCode2024.Day08 do
     end
   end
 
-  def part_b(_lines) do
+  def pair_to_antinodes_a({{x0, y0}, {x1, y1}}) do
+    x_diff = x1 - x0
+    y_diff = y1 - y0
+    [{x0 - x_diff, y0 - y_diff}, {x1 + x_diff, y1 + y_diff}]
+  end
+
+  def part_b(lines) do
+    do_it_all(lines, &pair_to_antinodes_b/1)
+  end
+
+  def pair_to_antinodes_b({{x0, y0}, {x1, y1}}) do
+    x_diff = x1 - x0
+    y_diff = y1 - y0
+    [{x0 - x_diff, y0 - y_diff}, {x1 + x_diff, y1 + y_diff}]
   end
 
   def a() do
--- test/day08_test.exs
+++ test/day08_test.exs
@@ -27,22 +27,33 @@ defmodule AdventOfCode2024.Day08Test do
     |> Enum.map(&String.trim/1)
   end
 
-  describe "antinodes/2" do
+  describe "antinodes A" do
     test "2 antennas, in bound antinodes" do
       dimensions = {10, 10}
       antennas = MapSet.new([{4, 3}, {5, 5}])
-      assert Day08.antinodes(antennas, dimensions) == MapSet.new([{3, 1}, {6, 7}])
+      assert Day08.antinodes(antennas, dimensions, &Day08.pair_to_antinodes_a/1) == MapSet.new([{3, 1}, {6, 7}])
     end
 
     test "3 antennas, some antinodes out of bounds" do
       dimensions = {10, 10}
       antennas = MapSet.new([{4, 3}, {5, 5}, {8, 4}])
-      assert Day08.antinodes(antennas, dimensions) == MapSet.new([{3, 1}, {6, 7}, {2, 6}, {0, 2}])
+
+      assert Day08.antinodes(antennas, dimensions, &Day08.pair_to_antinodes_a/1) ==
+               MapSet.new([{3, 1}, {6, 7}, {2, 6}, {0, 2}])
     end
   end
 
-  @tag :skip
   test "Day08 part B example" do
-    assert Day08.part_b(example_input()) == :something_else
+    assert Day08.part_b(example_input()) == 34
+  end
+
+  describe "antinodes B" do
+    test "3 antennas " do
+      dimensions = {10, 10}
+      antennas = MapSet.new([{0, 0}, {3, 1}, {1, 2}])
+
+      assert Day08.antinodes(antennas, dimensions, &Day08.pair_to_antinodes_b/1) ==
+               MapSet.new([{0, 0}, {3, 1}, {1, 2}, {5, 0}, {6, 2}, {9, 3}, {2, 4}, {3, 6}, {4, 8}])
+    end
   end
 end
