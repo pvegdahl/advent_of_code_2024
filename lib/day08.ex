d6c579732e8893ff543b13f689fa26e6cf6d8ec0
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-08 07:15:01 -0500

    Day 08 setup

---
+++ lib/day08.ex
@@ -0,0 +1,19 @@
+defmodule AdventOfCode2024.Day08 do
+  alias AdventOfCode2024.Helpers
+
+  def part_a(_lines) do
+  end
+
+  def part_b(_lines) do
+  end
+
+  def a() do
+    Helpers.file_to_lines!("inputs/day08.txt")
+    |> part_a()
+  end
+
+  def b() do
+    Helpers.file_to_lines!("inputs/day08.txt")
+    |> part_b()
+  end
+end
