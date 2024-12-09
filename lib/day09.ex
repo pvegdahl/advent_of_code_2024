a2114ee8dcad452f65e1cd310fdb86df60d1c91d
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-09 17:48:38 -0500

    Build the queue

--- lib/day09.ex
+++ lib/day09.ex
@@ -1,10 +1,24 @@
 defmodule AdventOfCode2024.Day09 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
+  def part_a(_line) do
   end
 
-  def part_b(_lines) do
+  def setup_queue(line) do
+    num_pairs =
+      line
+      |> String.codepoints()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(2, 2, [0])
+      |> Enum.with_index()
+
+    Enum.reduce(num_pairs, :queue.new(), fn {[file_size, empty_size], file_id}, q ->
+      (List.duplicate(file_id, file_size) ++ List.duplicate(nil, empty_size))
+      |> Enum.reduce(q, fn item, qq -> :queue.in(item, qq) end)
+    end)
+  end
+
+  def part_b(_line) do
   end
 
   def a() do
--- test/day09_test.exs
+++ test/day09_test.exs
@@ -7,6 +7,28 @@ defmodule AdventOfCode2024.Day09Test do
     assert Day09.part_a(example_input()) == 1928
   end
 
+  describe "setup_queue/1" do
+    test "Just one item" do
+      assert Day09.setup_queue("1") |> :queue.to_list() == [0]
+    end
+
+    test "longer single item" do
+      assert Day09.setup_queue("3") |> :queue.to_list() == [0, 0, 0]
+    end
+
+    test "item and blanks" do
+      assert Day09.setup_queue("32") |> :queue.to_list() == [0, 0, 0, nil, nil]
+    end
+
+    test "multiple items with blanks" do
+      assert Day09.setup_queue("11213") |> :queue.to_list() == [0, nil, 1, 1, nil, 2, 2, 2]
+    end
+
+    test "items with no blanks" do
+      assert Day09.setup_queue("10203") |> :queue.to_list() == [0, 1, 1, 2, 2, 2]
+    end
+  end
+
   defp example_input() do
     "2333133121414131402"
     |> String.trim()
