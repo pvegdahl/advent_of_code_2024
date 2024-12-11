b253d98e7bdd9738781390bcc8069e165d9e971c
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-10 19:33:12 -0500

    10A done

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,7 +1,46 @@
 defmodule AdventOfCode2024.Day10 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
+  def part_a(lines) do
+    grid = parse_grid(lines)
+    dimensions = Helpers.grid_dimensions(lines)
+
+    grid
+    |> Map.get(0)
+    |> Enum.map(&all_paths(&1, grid, dimensions))
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.sum()
+  end
+
+  def parse_grid(lines) do
+    lines
+    |> Helpers.parse_string_grid()
+    |> Map.new(fn {key, value} -> {String.to_integer(key), value} end)
+  end
+
+  defp all_paths(zero_point, grid, dimensions) do
+    Enum.reduce(1..9, [zero_point], fn next_value, points -> next_steps(points, next_value, grid, dimensions) end)
+  end
+
+  def next_steps(current_locations, next_value, grid, dimensions) do
+    candidates =
+      current_locations
+      |> Enum.flat_map(&neighbors(&1, dimensions))
+      |> MapSet.new()
+
+    viable = Map.get(grid, next_value)
+
+    MapSet.intersection(candidates, viable)
+  end
+
+  def neighbors({x, y}, dimensions) do
+    [
+      {x + 1, y},
+      {x - 1, y},
+      {x, y + 1},
+      {x, y - 1}
+    ]
+    |> Enum.filter(&Helpers.in_bounds?(&1, dimensions))
   end
 
   def part_b(_lines) do
--- test/day10_test.exs
+++ test/day10_test.exs
@@ -7,6 +7,20 @@ defmodule AdventOfCode2024.Day10Test do
     assert Day10.part_a(example_input()) == 36
   end
 
+  setup do
+    %{grid: Day10.parse_grid(example_input())}
+  end
+
+  describe "next_steps/4" do
+    test "goes from 0 to 1", %{grid: grid} do
+      assert Day10.next_steps([{2, 0}], 1, grid, {8, 8}) == MapSet.new([{2, 1}, {3, 0}])
+    end
+
+    test "Handles multiple input points", %{grid: grid} do
+      assert Day10.next_steps([{0, 1}, {6, 1}], 8, grid, {8, 8}) == MapSet.new([{0, 0}, {1, 1}, {0, 2}, {5, 1}])
+    end
+  end
+
   defp example_input() do
     """
     89010123
@@ -23,8 +37,7 @@ defmodule AdventOfCode2024.Day10Test do
     |> Enum.map(&String.trim/1)
   end
 
-  @tag :skip
   test "Day10 part B example" do
-    assert Day10.part_b(example_input()) == :something_else
+    assert Day10.part_b(example_input()) == 81
   end
 end
