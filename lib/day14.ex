3340c867909e9935cd831716f5412d8ba99a0cdd
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-15 17:26:06 -0500

    Day14A done

---
+++ lib/day14.ex
@@ -0,0 +1,63 @@
+defmodule AdventOfCode2024.Day14 do
+  alias AdventOfCode2024.Helpers
+
+  def part_a(lines, dimensions) do
+    final_states =
+      lines
+      |> parse_input()
+      |> move(dimensions, 100)
+
+    dimensions
+    |> quadrants()
+    |> Enum.map(fn quadrant -> points_in_ranges(final_states, quadrant) end)
+    |> Enum.map(&Enum.count/1)
+    |> Enum.product()
+  end
+
+  def parse_input(lines) do
+    Enum.map(lines, fn line ->
+      [x, y, vx, vy] =
+        Regex.run(~r/p=(\d+),(\d+) v=([\d-]+),([\d-]+)/, line)
+        |> Enum.drop(1)
+        |> Enum.map(&String.to_integer/1)
+
+      %{location: {x, y}, velocity: {vx, vy}}
+    end)
+  end
+
+  def move(location_velocity_pairs, {x_dim, y_dim}, n) do
+    Enum.map(location_velocity_pairs, fn %{location: {x, y}, velocity: {vx, vy}} ->
+      {Integer.mod(x + n * vx, x_dim), Integer.mod(y + n * vy, y_dim)}
+    end)
+  end
+
+  def part_b(_lines) do
+  end
+
+  def a() do
+    Helpers.file_to_lines!("inputs/day14.txt")
+    |> part_a({101, 103})
+  end
+
+  defp points_in_ranges(points, {x_range, y_range}) do
+    Enum.filter(points, fn {x, y} -> x in x_range and y in y_range end)
+  end
+
+  def quadrants({x_dim, y_dim}) do
+    x_mid = div(x_dim, 2)
+    y_mid = div(y_dim, 2)
+
+    x0 = 0..(x_mid - 1)
+    x1 = (x_mid + 1)..(x_dim - 1)
+
+    y0 = 0..(y_mid - 1)
+    y1 = (y_mid + 1)..(y_dim - 1)
+
+    [{x0, y0}, {x1, y0}, {x0, y1}, {x1, y1}]
+  end
+
+  def b() do
+    Helpers.file_to_lines!("inputs/day14.txt")
+    |> part_b()
+  end
+end
---
+++ test/day14_test.exs
@@ -0,0 +1,69 @@
+defmodule AdventOfCode2024.Day14Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode2024.Day14
+
+  test "Day14 part A example" do
+    assert Day14.part_a(example_input(), {11, 7}) == 12
+  end
+
+  defp example_input() do
+    """
+    p=0,4 v=3,-3
+    p=6,3 v=-1,-3
+    p=10,3 v=-1,2
+    p=2,0 v=2,-1
+    p=0,0 v=1,3
+    p=3,0 v=-2,-2
+    p=7,6 v=-1,-3
+    p=3,0 v=-1,-2
+    p=9,3 v=2,3
+    p=7,3 v=-1,2
+    p=2,4 v=2,-3
+    p=9,5 v=-3,-3
+    """
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+  end
+
+  describe "parse_input/1" do
+    test "example input" do
+      assert [
+               %{location: {0, 4}, velocity: {3, -3}},
+               %{location: {6, 3}, velocity: {-1, -3}},
+               %{location: {10, 3}, velocity: {-1, 2}},
+               %{location: {2, 0}, velocity: {2, -1}} | _
+             ] = Day14.parse_input(example_input())
+    end
+  end
+
+  describe "move/3" do
+    test "two points, single iteration" do
+      assert Day14.move(
+               [
+                 %{location: {0, 4}, velocity: {3, -3}},
+                 %{location: {6, 3}, velocity: {-1, -3}}
+               ],
+               {11, 7},
+               1
+             ) == [{3, 1}, {5, 0}]
+    end
+
+    test "two points, 10 iterations to cause wrapping" do
+      assert Day14.move(
+               [
+                 %{location: {0, 4}, velocity: {3, -3}},
+                 %{location: {6, 3}, velocity: {-1, -3}}
+               ],
+               {11, 7},
+               10
+             ) == [{8, 2}, {7, 1}]
+    end
+  end
+
+  @tag :skip
+  test "Day14 part B example" do
+    assert Day14.part_b(example_input()) == :something_else
+  end
+end
