b11b3cbbf93ce0aae75f83084b9ac316dc540c3c
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-17 06:42:40 -0500

    Input parsed

--- lib/day15.ex
+++ lib/day15.ex
@@ -4,6 +4,28 @@ defmodule AdventOfCode2024.Day15 do
   def part_a(_lines) do
   end
 
+  def parse_input(lines) do
+    {grid_lines, instruction_lines} = Enum.split_while(lines, &(String.length(&1) > 0))
+
+    grid =
+      grid_lines
+      |> Helpers.parse_string_grid()
+      |> reverse_grid_map()
+
+    instructions =
+      instruction_lines
+      |> Enum.join("")
+      |> String.graphemes()
+
+    {grid, instructions}
+  end
+
+  defp reverse_grid_map(grid_map) do
+    grid_map
+    |> Enum.flat_map(fn {character, points} -> Enum.map(points, &{&1, character}) end)
+    |> Map.new()
+  end
+
   def part_b(_lines) do
   end
 
--- test/day15_test.exs
+++ test/day15_test.exs
@@ -36,6 +36,22 @@ defmodule AdventOfCode2024.Day15Test do
     |> Enum.map(&String.trim/1)
   end
 
+  describe "parse_input/1" do
+    test "parses a simple (invalid) input" do
+      assert Day15.parse_input(["#..", "O#@", "O.O", "", "><^v"]) == {
+               %{
+                 {0, 0} => "#",
+                 {1, 1} => "#",
+                 {0, 1} => "O",
+                 {0, 2} => "O",
+                 {2, 2} => "O",
+                 {2, 1} => "@"
+               },
+               [">", "<", "^", "v"]
+             }
+    end
+  end
+
   @tag :skip
   test "Day15 part B example" do
     assert Day15.part_b(example_input()) == :something_else
