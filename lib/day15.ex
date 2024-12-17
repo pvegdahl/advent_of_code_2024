375fca230e1bbc36d95df366b8dfa33affb0f7d5
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-17 11:57:57 -0500

    Day15A done

--- lib/day15.ex
+++ lib/day15.ex
@@ -1,29 +1,78 @@
 defmodule AdventOfCode2024.Day15 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
+  def part_a(lines) do
+    {grid, instructions, starting_point} = parse_input(lines)
+
+    final_grid = all_moves(grid, starting_point, instructions)
+
+    final_grid
+    |> Enum.filter(fn {_location, type} -> type == "O" end)
+    |> Enum.map(fn {{x, y}, _type} -> x + 100 * y end)
+    |> Enum.sum()
   end
 
   def parse_input(lines) do
     {grid_lines, instruction_lines} = Enum.split_while(lines, &(String.length(&1) > 0))
 
-    grid =
-      grid_lines
-      |> Helpers.parse_string_grid()
-      |> reverse_grid_map()
+    type_to_points = Helpers.parse_string_grid(grid_lines)
+    grid = reverse_grid_map(type_to_points)
+    starting_point = Map.get(type_to_points, "@") |> Enum.at(0)
 
     instructions =
       instruction_lines
       |> Enum.join("")
       |> String.graphemes()
 
-    {grid, instructions}
+    {grid, instructions, starting_point}
   end
 
   defp reverse_grid_map(grid_map) do
     grid_map
     |> Enum.flat_map(fn {character, points} -> Enum.map(points, &{&1, character}) end)
     |> Map.new()
+  end
+
+  def move(grid, point, instruction) do
+    target = target_point(point, instruction)
+
+    case Map.get(grid, target) do
+      nil ->
+        {:ok, unconditional_move(grid, point, target)}
+
+      "#" ->
+        :error
+
+      "O" ->
+        case move(grid, target, instruction) do
+          {:ok, grid_with_pushed_box} -> {:ok, unconditional_move(grid_with_pushed_box, point, target)}
+          :error -> :error
+        end
+    end
+  end
+
+  defp target_point({x, y}, "^"), do: {x, y - 1}
+  defp target_point({x, y}, "v"), do: {x, y + 1}
+  defp target_point({x, y}, "<"), do: {x - 1, y}
+  defp target_point({x, y}, ">"), do: {x + 1, y}
+
+  defp unconditional_move(grid, source, target) do
+    source_type = Map.get(grid, source)
+
+    grid
+    |> Map.put(target, source_type)
+    |> Map.delete(source)
+  end
+
+  defp all_moves(grid, location, instructions)
+
+  defp all_moves(grid, _location, []), do: grid
+
+  defp all_moves(grid, location, [head_instruction | tail_instructions]) do
+    case move(grid, location, head_instruction) do
+      {:ok, updated_grid} -> all_moves(updated_grid, target_point(location, head_instruction), tail_instructions)
+      :error -> all_moves(grid, location, tail_instructions)
+    end
   end
 
   def part_b(_lines) do
--- test/day15_test.exs
+++ test/day15_test.exs
@@ -47,8 +47,50 @@ defmodule AdventOfCode2024.Day15Test do
                  {2, 2} => "O",
                  {2, 1} => "@"
                },
-               [">", "<", "^", "v"]
+               [">", "<", "^", "v"],
+               {2, 1}
              }
+    end
+  end
+
+  describe "move/3" do
+    setup do
+      {grid, _directions, _staring_point} = Day15.parse_input(example_input())
+      %{grid: grid}
+    end
+
+    test "A simple move into an empty space", %{grid: grid} do
+      assert {:ok, updated_grid} = Day15.move(grid, {4, 4}, "^")
+
+      refute Map.has_key?(updated_grid, {4, 4})
+      assert Map.get(updated_grid, {4, 3}) == "@"
+    end
+
+    test "Moving into a wall goes nowhere", %{grid: grid} do
+      assert :error == Day15.move(grid, {1, 5}, ">")
+    end
+
+    test "Push a box into an empty space", %{grid: grid} do
+      assert {:ok, updated_grid} = Day15.move(grid, {4, 4}, "<")
+
+      refute Map.has_key?(updated_grid, {4, 4})
+      assert Map.get(updated_grid, {3, 4}) == "@"
+      assert Map.get(updated_grid, {2, 4}) == "O"
+    end
+
+    test "Push multiple boxes at once", %{grid: grid} do
+      grid = Map.put(grid, {4, 5}, "O")
+      assert {:ok, updated_grid} = Day15.move(grid, {4, 4}, "v")
+
+      refute Map.has_key?(updated_grid, {4, 4})
+      assert Map.get(updated_grid, {4, 5}) == "@"
+      assert Map.get(updated_grid, {4, 6}) == "O"
+      assert Map.get(updated_grid, {4, 7}) == "O"
+    end
+
+    test "Cannot push boxes through the wall", %{grid: grid} do
+      grid = Map.merge(grid, %{{1, 4} => "O", {2, 4} => "O"})
+      assert :error == Day15.move(grid, {4, 4}, "<")
     end
   end
 
