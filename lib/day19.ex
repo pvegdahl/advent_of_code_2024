9ff1b81daa2dea857881374c79109af66f53bab3
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-22 11:44:37 -0500

    Seems to work, but much too slow

--- lib/day19.ex
+++ lib/day19.ex
@@ -1,8 +1,40 @@
 defmodule AdventOfCode2024.Day19 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    {towels, targets} = parse_input(lines)
+
+    max_towel_index = towels |> Enum.map(&String.length/1) |> Enum.max() |> then(&(&1 - 1))
+
+    targets
+    |> Enum.filter(fn target -> possible?(target, MapSet.new(towels), max_towel_index) end)
+    |> Enum.count()
+  end
+
+  def parse_input([towel_line, "" | rest]) do
+    towels =
+      towel_line
+      |> String.split(",")
+      |> Enum.map(&String.trim/1)
+
+    {towels, rest}
+  end
+
+  def possible?("", _towels, _max_towel_size), do: true
+
+  def possible?(target, towels, max_towel_index) do
+    towels
+    |> possible_towels(target, max_towel_index)
+    |> Enum.any?(fn towel ->
+      String.starts_with?(target, towel) and possible?(String.trim_leading(target, towel), towels, max_towel_index)
+    end)
+  end
+
+  defp possible_towels(towels, target, max_towel_index) do
+    0..max_towel_index
+    |> Enum.map(fn n -> String.slice(target, 0..n) end)
+    |> MapSet.new()
+    |> MapSet.intersection(towels)
   end
 
   def part_b(_lines) do
--- test/day19_test.exs
+++ test/day19_test.exs
@@ -25,6 +25,28 @@ defmodule AdventOfCode2024.Day19Test do
     |> Enum.map(&String.trim/1)
   end
 
+  describe "parse_input" do
+    test "example input" do
+      assert Day19.parse_input(example_input()) ==
+               {["r", "wr", "b", "g", "bwu", "rb", "gb", "br"],
+                ["brwrr", "bggr", "gbbr", "rrbgbr", "ubwu", "bwurrg", "brgr", "bbrgwb"]}
+    end
+  end
+
+  describe "possible?" do
+    setup do
+      %{towels: MapSet.new(["r", "wr", "b", "g", "bwu", "rb", "gb", "br"])}
+    end
+
+    test "yes!", %{towels: towels} do
+      assert Day19.possible?("bwurrg", towels, 6)
+    end
+
+    test "no!", %{towels: towels} do
+      refute Day19.possible?("bbrgwb", towels, 6)
+    end
+  end
+
   @tag :skip
   test "Day19 part B example" do
     assert Day19.part_b(example_input()) == 42
