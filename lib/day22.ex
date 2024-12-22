65610ea69da3be4ffc5fc30172f15addfda04d0a
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-22 10:14:50 -0500

    This works, but it is cheating a bit.
    There are theoretical inputs that would fail. Unlikely, but possible.

--- lib/day22.ex
+++ lib/day22.ex
@@ -34,11 +34,27 @@ defmodule AdventOfCode2024.Day22 do
 
   defp prune(num), do: Integer.mod(num, 16_777_216)
 
-  def part_b(lines) do
-    lines
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&change_sequence(&1, 2000))
-    |> try_everything()
+  def part_b(lines, threshold) do
+    change_sequences =
+      lines
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.map(&change_sequence(&1, 2000))
+
+    bid_sequences = likely_bid_sequences(change_sequences, threshold)
+
+    chunk_size = Enum.count(bid_sequences) |> div(11)
+
+    bid_sequences
+    |> Enum.chunk_every(chunk_size)
+    |> Task.async_stream(fn chunk -> one_chunk(change_sequences, chunk) end, timeout: :infinity)
+    |> Enum.map(fn {:ok, result} -> result end)
+    |> Enum.max()
+  end
+
+  defp one_chunk(change_sequences, bid_sequence_chunk) do
+    bid_sequence_chunk
+    |> Enum.map(&score_one_bid(change_sequences, &1))
+    |> Enum.max()
   end
 
   def change_sequence(secret_number, n) do
@@ -56,19 +72,26 @@ defmodule AdventOfCode2024.Day22 do
     |> Enum.sum()
   end
 
-  defp all_bid_sequences do
-    for a <- -9..9, b <- -9..9, c <- -9..9, d <- -9..9, do: [a, b, c, d]
+  defp likely_bid_sequences(change_sequences, threshold) do
+    # There are ~40k unique sequences in the 2K lists. A number of them show up > 300 times. Most show up less than 200.
+    # Let's focus on the likely ones that show up at least <threshold> times.
+    change_sequences
+    |> Enum.flat_map(&bid_sequences_for_one_list/1)
+    |> Enum.frequencies()
+    |> Enum.reject(fn {_key, value} -> value < threshold end)
+    |> Enum.map(fn {key, _value} -> key end)
+  end
+
+  defp bid_sequences_for_one_list(change_sequence) do
+    change_sequence
+    |> Enum.map(&elem(&1, 1))
+    |> Enum.chunk_every(4, 1, :discard)
+    |> Enum.uniq()
   end
 
   def bid([_, _, _], _bid_sequence), do: 0
   def bid([{_, a}, {_, b}, {_, c}, {bananas, d} | _], [a, b, c, d]), do: bananas
   def bid([_head | tail], bid_sequence), do: bid(tail, bid_sequence)
-
-  defp try_everything(change_sequences) do
-    all_bid_sequences()
-    |> Enum.map(&score_one_bid(change_sequences, &1))
-    |> Enum.max()
-  end
 
   def a() do
     Helpers.file_to_lines!("inputs/day22.txt")
@@ -77,6 +100,6 @@ defmodule AdventOfCode2024.Day22 do
 
   def b() do
     Helpers.file_to_lines!("inputs/day22.txt")
-    |> part_b()
+    |> part_b(200)
   end
 end
