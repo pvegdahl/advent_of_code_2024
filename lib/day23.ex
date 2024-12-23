8b4f2a8ac4bf97b984a79dc9fa39eae0732633ef
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-23 09:46:30 -0500

    Done with 23A

--- lib/day23.ex
+++ lib/day23.ex
@@ -1,8 +1,46 @@
 defmodule AdventOfCode2024.Day23 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    mapping = parse_input(lines)
+
+    starts_with_t = keys_starting_with(mapping, "t")
+
+    starts_with_t
+    |> Enum.flat_map(fn key -> find_triples_for(key, mapping) end)
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  def parse_input(lines) do
+    lines
+    |> Enum.map(&String.split(&1, "-"))
+    |> Enum.flat_map(fn [a, b] -> [{a, b}, {b, a}] end)
+    |> Enum.group_by(fn {key, _value} -> key end, fn {_key, value} -> value end)
+    |> Map.new(fn {key, values} -> {key, MapSet.new(values)} end)
+  end
+
+  defp keys_starting_with(mapping, starting_pattern) do
+    mapping
+    |> Map.keys()
+    |> Enum.filter(fn key -> String.starts_with?(key, starting_pattern) end)
+  end
+
+  def find_triples_for(target, mapping) do
+    target_connections = Map.get(mapping, target)
+
+    target_connections
+    |> Enum.flat_map(fn connection ->
+      mapping
+      |> Map.get(connection)
+      |> MapSet.intersection(target_connections)
+      |> Enum.map(fn mutual_connection ->
+        [target, connection, mutual_connection]
+        |> Enum.sort()
+        |> List.to_tuple()
+      end)
+    end)
+    |> Enum.uniq()
   end
 
   def part_b(_lines) do
--- test/day23_test.exs
+++ test/day23_test.exs
@@ -47,6 +47,31 @@ defmodule AdventOfCode2024.Day23Test do
     |> Enum.map(&String.trim/1)
   end
 
+  describe "parse_input/1" do
+    test "small input -- I'm not crazy enough to try to figure out the whole example input" do
+      assert Day23.parse_input(["ab-cd", "ab-ef", "ab-gh", "gh-cd"]) == %{
+               "ab" => MapSet.new(["cd", "ef", "gh"]),
+               "cd" => MapSet.new(["ab", "gh"]),
+               "ef" => MapSet.new(["ab"]),
+               "gh" => MapSet.new(["ab", "cd"])
+             }
+    end
+  end
+
+  describe "find_triples_for/2" do
+    setup do
+      %{mapping: Day23.parse_input(example_input())}
+    end
+
+    test "tb has one", %{mapping: mapping} do
+      assert Day23.find_triples_for("tb", mapping) == [{"tb", "vc", "wq"}]
+    end
+
+    test "ta has three", %{mapping: mapping} do
+      assert Day23.find_triples_for("ta", mapping) == [{"co", "de", "ta"}, {"co", "ka", "ta"}, {"de", "ka", "ta"}]
+    end
+  end
+
   @tag :skip
   test "Day23 part B example" do
     assert Day23.part_b(example_input()) == 42
