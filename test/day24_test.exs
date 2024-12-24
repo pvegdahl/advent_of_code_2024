363a5c584fd48853e7824355885f96a74d0b11f2
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-24 09:09:50 -0500

    Christmas Eve setup

---
+++ test/day24_test.exs
@@ -0,0 +1,69 @@
+defmodule AdventOfCode2024.Day24Test do
+  use ExUnit.Case, async: true
+
+  alias AdventOfCode2024.Day24
+
+  test "Day24 part A example" do
+    assert Day24.part_a(example_input()) == 2024
+  end
+
+  defp example_input() do
+    """
+    x00: 1
+    x01: 0
+    x02: 1
+    x03: 1
+    x04: 0
+    y00: 1
+    y01: 1
+    y02: 1
+    y03: 1
+    y04: 1
+
+    ntg XOR fgs -> mjb
+    y02 OR x01 -> tnw
+    kwq OR kpj -> z05
+    x00 OR x03 -> fst
+    tgd XOR rvg -> z01
+    vdt OR tnw -> bfw
+    bfw AND frj -> z10
+    ffh OR nrd -> bqk
+    y00 AND y03 -> djm
+    y03 OR y00 -> psh
+    bqk OR frj -> z08
+    tnw OR fst -> frj
+    gnj AND tgd -> z11
+    bfw XOR mjb -> z00
+    x03 OR x00 -> vdt
+    gnj AND wpb -> z02
+    x04 AND y00 -> kjc
+    djm OR pbm -> qhw
+    nrd AND vdt -> hwm
+    kjc AND fst -> rvg
+    y04 OR y02 -> fgs
+    y01 AND x02 -> pbm
+    ntg OR kjc -> kwq
+    psh XOR fgs -> tgd
+    qhw XOR tgd -> z09
+    pbm OR djm -> kpj
+    x03 XOR y03 -> ffh
+    x00 XOR y04 -> ntg
+    bfw OR bqk -> z06
+    nrd XOR fgs -> wpb
+    frj XOR qhw -> z04
+    bqk OR frj -> z07
+    y03 OR x01 -> nrd
+    hwm AND bqk -> z03
+    tgd XOR rvg -> z12
+    tnw OR pbm -> gnj
+    """
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+  end
+
+  @tag :skip
+  test "Day24 part B example" do
+    assert Day24.part_b(example_input()) == 42
+  end
+end
