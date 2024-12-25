930538863e8f413a4c31f61dcbb11061893b4fcb
Philip Vegdahl <pvegdahl@gmail.com>
2024-12-25 10:21:31 -0500

    Christmas day A done!

--- lib/day25.ex
+++ lib/day25.ex
@@ -1,8 +1,45 @@
 defmodule AdventOfCode2024.Day25 do
   alias AdventOfCode2024.Helpers
 
-  def part_a(_lines) do
-    -1
+  def part_a(lines) do
+    %{keys: keys, locks: locks} = parse_input(lines)
+
+    for key <- keys, lock <- locks do
+      not overlap?(key, lock)
+    end
+    |> Enum.count(&Function.identity/1)
+  end
+
+  def parse_input(lines) do
+    chunks =
+      lines
+      |> Enum.chunk_by(fn line -> line == "" end)
+      |> Enum.reject(fn line_group -> line_group == [""] end)
+
+    %{key: key_chunks, lock: lock_chunks} = Enum.group_by(chunks, &chunk_type/1)
+
+    locks = Enum.map(lock_chunks, &parse_lock_or_key/1)
+    keys = Enum.map(key_chunks, &parse_lock_or_key/1)
+
+    %{locks: locks, keys: keys}
+  end
+
+  defp chunk_type(["#####" | _]), do: :lock
+  defp chunk_type(_), do: :key
+
+  defp parse_lock_or_key(lines) do
+    lines
+    |> Enum.map(&String.codepoints/1)
+    |> Enum.zip_reduce([], fn elements, acc ->
+      [Enum.count(elements, &(&1 == "#")) - 1 | acc]
+    end)
+    |> Enum.reverse()
+  end
+
+  defp overlap?(key, lock) do
+    [key, lock]
+    |> Enum.zip()
+    |> Enum.any?(fn {k, l} -> k + l > 5 end)
   end
 
   def part_b(_lines) do
--- test/day25_test.exs
+++ test/day25_test.exs
@@ -54,6 +54,15 @@ defmodule AdventOfCode2024.Day25Test do
     |> Enum.map(&String.trim/1)
   end
 
+  describe "parse_input/1" do
+    test "example_input" do
+      assert %{
+               locks: [[0, 5, 3, 4, 3], [1, 2, 0, 5, 3]],
+               keys: [[5, 0, 2, 1, 3], [4, 3, 4, 0, 2], [3, 0, 2, 0, 1]]
+             } == Day25.parse_input(example_input())
+    end
+  end
+
   @tag :skip
   test "Day25 part B example" do
     assert Day25.part_b(example_input()) == 42
